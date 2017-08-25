68f33c802e5abfde06b3a492987f367cf1353a92
Nic Wolff <nwolff@hearst.com>
2017-08-25 16:58:08 -0400

    Revert "Merge pull request #5 from HearstCorp/PDT-250-Redis-circuit-breaker"
    This reverts commit fb4475cdcdbf757f3bb86e9e2ca9152c93d3bfdd, reversing
    changes made to 4eb60b3e0f699f459f544a81c42ad50e92418373.

--- cacheops/redis.py
+++ cacheops/redis.py
@@ -1,46 +1,28 @@
 from __future__ import absolute_import
 import warnings
 import six
-from time import time
-import logging
+import sys
+import traceback
 
 from funcy import decorator, identity, memoize
 import redis
+from django.core.exceptions import ImproperlyConfigured
 import random
 
 from .conf import settings
 
-logger = logging.getLogger(__name__)
-
-# Global circuit-breaker flag. Values:
-#   None = circuit breaker closed, Redis calls are attempted.
-#   <time_seconds> = circuit breaker open, Redis calls are skipped.
-circuit_breaker_opened = None
 
 if settings.CACHEOPS_DEGRADE_ON_FAILURE:
     @decorator
     def handle_connection_failure(call):
-        """Skip Redis calls for a configurable period after a timeout."""
-        global circuit_breaker_opened
-        if settings.FEATURE_CACHEOPS_CIRCUIT_BREAKER and circuit_breaker_opened:
-            # Circuit breaker is open! Should we close it yet?
-            if time() - circuit_breaker_opened > settings.REDIS_CIRCUIT_BREAKER_RESET_SECONDS:
-                # Yes, let's close the circuit breaker
-                logger.info("Closing Redis circuit breaker")
-                circuit_breaker_opened = None
-            else:
-                # No, just skip this Redis call
-                logger.debug("Redis circuit breaker is open! Skipping Redis call")
-                return
         try:
             return call()
-        except redis.RedisError as e:
-            # Redis timed out! Let's open the circuit breaker
-            logger.warn("The cacheops cache is unreachable! Error: %s" % e)
-            logger.info("Opening Redis circuit breaker")
-            circuit_breaker_opened = time()
+        except redis.ConnectionError as e:
+            warnings.warn("The cacheops cache is unreachable! Error: %s" % e, RuntimeWarning)
+        except redis.TimeoutError as e:
+            warnings.warn("The cacheops cache timed out! Error: %s" % e, RuntimeWarning)
         except Exception as e:
-            logger.warn(e)
+            warnings.warn("".join(traceback.format_exception(*sys.exc_info())))
 else:
     handle_connection_failure = identity
 
